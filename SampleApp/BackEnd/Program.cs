e53f991cd12202a658703cd0a2b0273a427f0e3b
Edwin Bartlett <67263400+teddzyb@users.noreply.github.com>
2025-04-27 09:59:48 +0000

    Add WeatherForecastByCity endpoint and corresponding record

--- SampleApp/BackEnd/Program.cs
+++ SampleApp/BackEnd/Program.cs
@@ -46,9 +46,31 @@ app.MapGet("/weatherforecast", () =>
 })
 .WithName("GetWeatherForecast");
 
+// Create a new endpoint named /WeatherForecastByCity/{city}, that accepts a city name in the urls as a paremeter and generates a random forecast for that city
+app.MapGet("/WeatherForecastByCity/{city}", (string city) =>
+{
+    var forecast = new WeatherForecastByCity
+    (
+        city,
+        DateOnly.FromDateTime(DateTime.Now),
+        Random.Shared.Next(-20, 55),
+        summaries[Random.Shared.Next(summaries.Length)]
+    );
+    return forecast;
+})
+.WithName("GetWeatherForecastByCity");
+
 app.Run();
 
 internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
 {
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 }
+
+// create a new internal record named WeatherForecastByCity that request the following parameters: City, Date, TemperatureC, Summary
+internal record WeatherForecastByCity(string City, DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
+
+
